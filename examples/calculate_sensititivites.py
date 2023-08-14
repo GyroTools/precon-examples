e7814f750baeff644adcc82da115b1d97c0d4afe
gitlab-runner <gitlab@gyrotools.com>
2023-08-14 14:12:35 +0000

    Update .gitlab-ci.yml

--- examples/calculate_sensititivites.py
+++ examples/calculate_sensititivites.py
@@ -10,9 +10,11 @@ parser.add_argument('refscan', help='path to the raw or lab file of the sense re
 parser.add_argument('target_scan', help='path to the raw or lab file of the target scan')
 parser.add_argument('--output-path', default='./', help='path where the output is saved')
 parser.add_argument('--match-target-size', action='store_true', help='if given then the size of the sensitivities matches the one of the target data')
+parser.add_argument('--fov', nargs="+", type=float, default=None, help='the FOV of the sensitivity maps')
+parser.add_argument('--output_size', nargs="+", type=int, default=None, help='the putput size of the sensitivities')
 args = parser.parse_args()
 
-s = calculate_sensitivities(Path(args.refscan), Path(args.target_scan), stack=0, mix=0, match_target_size=args.match_target_size)
+s = calculate_sensitivities(Path(args.refscan), Path(args.target_scan), stack=0, mix=0, match_target_size=args.match_target_size, fov=args.fov, output_size=args.output_size)
 
 mdic = {'qbc': s.bodycoil, 'coil': s.surfacecoil, 'sensitivity': s.sensitivity, 'psi': s.psi}
 savemat(Path(args.output_path) / 'sense.mat', mdic)
